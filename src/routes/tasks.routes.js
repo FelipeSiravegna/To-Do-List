dd9aa72a7e0939b07a994593b96940b06c609461
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-08-24 20:12:54 -0300

    Conection with database established

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/tasks", (req, res) => {
-  res.send("Retrieving a list of tasks");
+  res.send("Retrieving a list of tasks")
 });
 
 router.get("/tasks/:id", (req, res) => {
