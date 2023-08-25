f9d641aec7fdf19eaebd15952ec53e4cef10e499
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-08-25 19:05:05 -0300

    Get a specific task controller

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -3,7 +3,7 @@ const pool = require("../db");
 const getAllTasks = async (req, res) => {
   try {
     const allTasks = await pool.query("SELECT * FROM task");
-    
+
     res.json(allTasks.rows);
   } catch (error) {
     console.log(error.message);
@@ -12,7 +12,22 @@ const getAllTasks = async (req, res) => {
 };
 
 const getTask = async (req, res) => {
-  res.send("Retrieving a task");
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.json(result.rows[0])
+  } catch (error) {
+    console.log(error.message);
+    res.json({ error: error.message });
+  }
 };
 
 const createTask = async (req, res) => {
