8ee66e231064c2c6e676cf27ba1899b77067c8ca
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-08-26 02:06:35 -0300

    Put task controller

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -69,7 +69,26 @@ const deleteTask = async (req, res) => {
 };
 
 const updateTask = async (req, res) => {
-  res.send("Updating a task");
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  try {
+    const result = await pool.query(
+      "UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *",
+      [title, description, id]
+    );
+
+    if(result.rows.length === 0){
+      res.status(404).json({
+        message: "Task not found"
+      })
+    }
+    
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ error: error.message });
+  }
 };
 
 module.exports = {
