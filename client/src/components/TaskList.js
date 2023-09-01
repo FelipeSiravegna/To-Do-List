cde640a3d43bdb9126e884449cd3382096489e69
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-09-01 18:57:04 -0300

    Tasks are now listed

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import {Card, CardContent, Typography} from '@mui/material'
+
 export default function TaskList() {
+
+    const [tasks, setTasks] = useState([])
+
+    const getTasks = async () => {
+        const response = await fetch('http://localhost:3000/tasks')
+        const data = await response.json()
+        setTasks(data);
+    }
+
+    useEffect(() => {
+        getTasks()
+    }, [])
+
     return (
-        <div>
-            <h1>TaskList</h1>
-        </div>
+        <>
+            <h1>Task list</h1>
+            {
+                tasks.map((task) => (
+                    <Card>
+                        <CardContent>
+                            <Typography>{task.title}</Typography>
+                            <Typography>{task.description}</Typography>
+                        </CardContent>
+                    </Card>
+                ))
+            }
+        </>
     );
 }
