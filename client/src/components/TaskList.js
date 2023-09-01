9569866d1622b1d5d68c24d554382b4786b9bca0
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-09-01 19:12:42 -0300

    Task List styles added + Delete and edit buttons added

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,33 +1,65 @@
 import { useEffect, useState } from "react";
-import {Card, CardContent, Typography} from '@mui/material'
+import { Button, Card, CardContent, Typography } from "@mui/material";
 
 export default function TaskList() {
-
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState([]);
 
     const getTasks = async () => {
-        const response = await fetch('http://localhost:3000/tasks')
-        const data = await response.json()
+        const response = await fetch("http://localhost:3000/tasks");
+        const data = await response.json();
         setTasks(data);
-    }
+    };
 
     useEffect(() => {
-        getTasks()
-    }, [])
+        getTasks();
+    }, []);
 
     return (
         <>
             <h1>Task list</h1>
-            {
-                tasks.map((task) => (
-                    <Card>
-                        <CardContent>
+            {tasks.map((task) => (
+                <Card
+                    style={{
+                        marginBottom: "0.7rem",
+                        backgroundColor: "#1e272e",
+                    }}
+                >
+                    <CardContent
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <div style={{
+                            color: "white"
+                        }}>
                             <Typography>{task.title}</Typography>
                             <Typography>{task.description}</Typography>
-                        </CardContent>
-                    </Card>
-                ))
-            }
+                        </div>
+
+                        <div>
+                            <Button
+                                variant="contained"
+                                color="inherit"
+                                onClick={() => console.log("Editing")}
+                            >
+                                Edit
+                            </Button>
+
+                            <Button
+                                variant="contained"
+                                color="warning"
+                                onClick={() => console.log("Deleting")}
+                                style={{
+                                    marginLeft: "0.5rem"
+                                }}
+                            >
+                                Delete
+                            </Button>
+                        </div>
+                    </CardContent>
+                </Card>
+            ))}
         </>
     );
 }
