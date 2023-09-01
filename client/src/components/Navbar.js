7e0bb0177a725a5c7a028b4bce496a11fdf04089
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-09-01 19:00:53 -0300

    Navbar background color changed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,12 +6,14 @@ export default function Navbar() {
     const navigate = useNavigate()
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1 }} style={{
+            backgroundColor: "#1e272e"
+        }}>
             <AppBar position="static" color="transparent">
                 <Container>
                     <Toolbar>
                         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                            <Link to="/" style={{textDecoration: "none", color: "black"}}>PERN Stack</Link>
+                            <Link to="/" style={{textDecoration: "none", color: "white"}}>PERN Stack</Link>
                         </Typography>
 
                         <Button variant="contained" color="primary" onClick={() => navigate('/tasks/new')}>
